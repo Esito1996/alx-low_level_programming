817f307da7a6ae88244eac6829721d80bf1e3c99
Esito1996 <esromabrha7@gmail.com>
2022-06-30 09:43:00 -0700

    haleluya

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
-int main(){
-
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-}	
+int main(void)
+{
+put("\"programming is like building a multilingual puzzle");
+return (0);
+}
