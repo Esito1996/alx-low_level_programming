2ee31c0b17cbb579b11cfed9ad238082a043ee3c
Esito1996 <esromabrha7@gmail.com>
2022-07-05 11:22:39 -0700

    j

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,23 +1,24 @@
-int _putchar(char k);
+#ifndef MAIN_H
+#define MAIN_H
 
+/*
+ *  * File: main.h
+ *   *
+ *    * Desc: Header file containing declartions for all function
+ *     */
+
+int _putchar(char c);
 void print_alphabet(void);
-
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
 int print_sign(int n);
-
-int _abs(int a);
-
-int print_last_digit(int a);
-
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
+void time_teble(void);
+int add(int, int);
 void print_to_98(int n);
+void print_time_table(int n);
+
+#endif
