c8275dd1243a134a5755c2f21f8b13332302b26a
Esito1996 <esromabrha7@gmail.com>
2022-07-22 14:26:56 -0700

    kj

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing the prototypes of all functions
- *       included in the static library libholberton.a.
- */
+/*copy list from task*/
 
 int _putchar(char c);
 int _islower(int c);
